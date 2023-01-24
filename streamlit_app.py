b852618dcc6532a8333b836f1e9c7e17aa0fa6cf
Sivaa16 <123355453+Sivaa16@users.noreply.github.com>
2023-01-24 20:22:59 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,9 +32,7 @@ def get_fruityvice_data(this_fruit_choice):
    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
    streamlit.dataframe(fruityvice_normalized)
    
-   
 streamlit.header("Fruityvice Fruit Advice!")
-
 try: 
    fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
    if not fruit_choice:
@@ -42,8 +40,6 @@ try:
    else:
        back_from_function = get_fruityvice_data(fruit_choice)
        streamlit.dataframe(back_from_function)
-       
-       
 
 except urlerror as e:
      streamit.error()
@@ -58,7 +54,6 @@ if streamlit.button('Get fruit load list'):
    my_cnx.close()
    my_data_rows = get_fruit_load_list()
    streamlit.dataframe(my_data_rows)
-   
    
 def insert_row_snowflake(new_fruit):
     with my_cnx.cursor as my_cur:
